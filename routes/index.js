e8861eb4c3286c067d2486f58f791a78e58ee0ce
bcushenberry <cushenberry@gmail.com>
2024-07-14 05:08:41 +0900

    fixing route issue

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 
 router.get('/', (req, res) => {
     //#swagger tags=["Hello World!"]
-    res.send('Hello World!');
+    res.send('CSE341 Project 2');
 });
 
-router.use('/weapons', require('../controllers/weapons'));
-router.use('/armor', require('../controllers/armor'));
+router.use('/weapons', require('./weapons'));
+router.use('/armor', require('./armor'));
 router.use('/api-docs', require('./swagger'));
 
 module.exports = router;
