65eea9116a8f25d6ee5b0fd00556ca2fde7f6995
bcushenberry <cushenberry@gmail.com>
2024-07-14 08:12:08 +0900

    added error handler

--- routes/armor.js
+++ routes/armor.js
@@ -1,20 +1,21 @@
 const router = require('express').Router();
 const armorController = require('../controllers/armor');
 const { armorValidationRules, validate } = require('../utilities/armorValidation');
+const utilities = require('../utilities/')
 
 // List all armors (GET)
-router.get('/', armorController.getAll);
+router.get('/', utilities.errorHandler(armorController.getAll));
 
 // Show individual armor (GET)
-router.get('/:id', armorController.getById);
+router.get('/:id', utilities.errorHandler(armorController.getById));
 
 // Create new armor (POST)
-router.post('/', armorValidationRules(), validate, armorController.createArmor);
+router.post('/', armorValidationRules(), validate, utilities.errorHandler(armorController.createArmor));
 
 // Update armor data (PUT)
-router.put('/:id', armorValidationRules(), validate, armorController.updateArmor);
+router.put('/:id', armorValidationRules(), validate, utilities.errorHandler(armorController.updateArmor));
 
 // Delete individual armor (DELETE)
-router.delete('/:id', armorController.deleteArmor);
+router.delete('/:id', utilities.errorHandler(armorController.deleteArmor));
 
 module.exports = router;
--- routes/weapons.js
+++ routes/weapons.js
@@ -1,20 +1,21 @@
 const router = require('express').Router();
 const weaponController = require('../controllers/weapons');
 const { weaponValidationRules, validate } = require('../utilities/weaponValidation');
+const utilities = require('../utilities/')
 
 // List all weapons (GET)
-router.get('/', weaponController.getAll);
+router.get('/', utilities.errorHandler(weaponController.getAll));
 
 // Show individual weapon (GET)
-router.get('/:id', weaponController.getById);
+router.get('/:id', utilities.errorHandler(weaponController.getById));
 
 // Create new weapon (POST)
-router.post('/', weaponValidationRules(), validate, weaponController.createWeapon);
+router.post('/', weaponValidationRules(), validate, utilities.errorHandler(weaponController.createWeapon));
 
 // Update weapon data (PUT)
-router.put('/:id', weaponValidationRules(), validate, weaponController.updateWeapon);
+router.put('/:id', weaponValidationRules(), validate, utilities.errorHandler(weaponController.updateWeapon));
 
 // Delete individual weapon (DELETE)
-router.delete('/:id', weaponController.deleteWeapon);
+router.delete('/:id', utilities.errorHandler(weaponController.deleteWeapon));
 
 module.exports = router;
