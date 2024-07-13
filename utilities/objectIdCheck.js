9d18b3825d39d6a62807ac4262bd4ce3e640ac42
bcushenberry <cushenberry@gmail.com>
2024-07-14 06:51:49 +0900

    fixing an issue with a variable

--- utilities/objectIdCheck.js
+++ utilities/objectIdCheck.js
@@ -1,8 +1,9 @@
 const { ObjectId } = require('mongodb');
 const createError = require('http-errors');
+const database = require('../db/database');
 
 const checkIdValidity = async (id, db, type) => {
-    const result = await mongodb.getDb().db().collection(db).findOne({_id: id});
+    const result = await database.getDb().db().collection(db).findOne({_id: id});
 
     if (!ObjectId.isValid(id)) {
         throw createError(400, 'Invalid ID format. Must be a valid ObjectId');
